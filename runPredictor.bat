8b7d2a19f20bf62bffb4bb661cd373787633ff69
kobegatti <kobegatti@gmail.com>
2022-10-14 00:31:29 -0700

    uploading data

---
+++ runPredictor.bat
@@ -0,0 +1,2 @@
+cd "C:\users\kobeg\python\NBAGamePredictor\2022-23"
+"Scripts\python.exe" "main.py"
---
+++ uploadFilesToGitHub.bat
@@ -0,0 +1,5 @@
+cd "C:\users\kobeg\python\NBAGamePredictor"
+git status
+git add .
+git commit -m "uploading data"
+git push
