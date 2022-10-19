ad125420b179997d6b601104997665dfde0c1e39
kobegatti <kobegatti@gmail.com>
2022-10-19 14:52:12 -0700

    bat files

--- getCurrentSeasonData.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\users\kobeg\python\NBAGamePredictor\2022-23"
-"Scripts\python.exe" "CurrentSeasonData.py"
--- uploadFilesToGitHub.bat
+++
@@ -1,6 +0,0 @@
-cd "C:\users\kobeg\python\NBAGamePredictor"
-git checkout gh-pages
-git status
-git add .
-git commit -m "uploading data"
-git push
